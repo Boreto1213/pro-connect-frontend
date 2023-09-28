b55cf76d6f10a9586929712bdcd1bf62f4fc2983
Boris Damianov <borisvd4@gmail.com>
2023-09-28 19:48:53 +0200

    code clean up and some minor changes

--- src/pages/AuthLayout.tsx
+++ src/pages/AuthLayout.tsx
@@ -1,52 +1,15 @@
 import { FC } from 'react'
 import { Outlet } from 'react-router-dom'
 import globalNetwork from '../assets/global-network.png'
+import { Icons } from '../components/Icons'
 
 interface AuthLayoutProps {}
 
 const AuthLayout: FC<AuthLayoutProps> = ({}) => {
   return (
     <div className='flex 2xl:justify-end justify-evenly items-center w-full h-screen bg-slate-700 overflow-hidden relative px-16 2xl:gap-[400px]'>
-      <div className='absolute top-6 left-6 z-10 flex justify-center items-center gap-2'>
-        <svg
-          height='60px'
-          width='60px'
-          version='1.1'
-          id='Capa_1'
-          xmlns='http://www.w3.org/2000/svg'
-          xmlnsXlink='http://www.w3.org/1999/xlink'
-          viewBox='0 0 59 59'
-          xmlSpace='preserve'
-          fill='#000000'
-        >
-          <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
-          <g
-            id='SVGRepo_tracerCarrier'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          ></g>
-          <g id='SVGRepo_iconCarrier'>
-            {' '}
-            <g>
-              {' '}
-              <g>
-                {' '}
-                <path
-                  style={{ fill: '#14b8a5' }}
-                  d='M13,16.81c0-2.104,1.706-3.81,3.81-3.81H47V4.81C47,2.706,45.294,1,43.19,1H4.81 C2.706,1,1,2.706,1,4.81v38.38C1,45.294,2.706,47,4.81,47H13V16.81z'
-                ></path>{' '}
-                <path
-                  style={{ fill: '#26B99A' }}
-                  d='M14,48H4.81C2.157,48,0,45.842,0,43.19V4.81C0,2.158,2.157,0,4.81,0H43.19 C45.843,0,48,2.158,48,4.81V14H16.81C15.261,14,14,15.261,14,16.81V48z M4.81,2C3.261,2,2,3.261,2,4.81v38.38 C2,44.739,3.261,46,4.81,46H12V16.81c0-2.652,2.157-4.81,4.81-4.81H46V4.81C46,3.261,44.739,2,43.19,2H4.81z'
-                ></path>{' '}
-              </g>{' '}
-              <path
-                style={{ fill: '#64748b' }}
-                d='M55.19,59H16.81C14.706,59,13,57.294,13,55.19V16.81c0-2.104,1.706-3.81,3.81-3.81h38.38 c2.104,0,3.81,1.706,3.81,3.81v38.38C59,57.294,57.294,59,55.19,59z'
-              ></path>{' '}
-            </g>{' '}
-          </g>
-        </svg>
+      <div className='absolute top-5 left-8 z-10 flex justify-center items-center gap-2'>
+        <Icons.Logo />
         <h1 className='text-4xl font-semibold text-teal-500'>
           Pro<span className='text-slate-500'>Connect</span>
         </h1>
--- src/pages/DashboardLayout.tsx
+++ src/pages/DashboardLayout.tsx
@@ -1,13 +1,10 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import { Icons } from '../components/Icons'
 import { SideBarOption } from '../types/sidebar-options'
 import SidebarOptions from '../components/SidebarOptions'
 
-interface DashboardLayoutProps {}
 
-
-
-const DashboardLayout: FC<DashboardLayoutProps> = ({}) => {
+const DashboardLayout: FC = () => {
   const sidebarOptions: SideBarOption[] = [
     {id: 1, name: 'Home', href: '/', icon: 'Home'},
     {id: 4, name: 'Chats', href: '/chats', icon: 'MessageCircle'},
@@ -20,7 +17,7 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({}) => {
   return (
     <>
       <div className='flex justify-between items-center w-screen h-[100px] px-8 border-b-1 border-gray-200'>
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center gap-2'>
         <Icons.Logo />
         <h1 className='text-4xl font-semibold text-teal-500'>
           Pro<span className='text-slate-500'>Connect</span>
@@ -29,8 +26,8 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({}) => {
       </div>
       <div className='relative h-[calc(100vh-100px)]'>
         <SidebarOptions sidebarOptions={sidebarOptions} />
-        <div className='inline-flex h-full w-full  pl-[350px]'>
-          <div id='dashboard-content' className='w-full h-ful'>
+        <div className='inline-flex h-full w-full  pl-[320px]'>
+          <div id='dashboard-content' className='w-full h-full bg-gray-50 flex justify-center items-center text-4xl text-slate-800'>
             Content
           </div>
         </div>
